7805a1135c9a3432ae1da59829d0d2fb4291c69a
Rugal Tu <john2653589@gmail.com>
2024-10-09 17:57:56 +0800

    Rugal Update

--- Rugal.DotNetLib.Core/DateConvert/DateOnlyExtention.cs
+++ Rugal.DotNetLib.Core/DateConvert/DateOnlyExtention.cs
@@ -2,12 +2,12 @@
 {
     public static class DateOnlyExtention
     {
-        public static DateTime ToDateTime(DateOnly ConvertDate)
+        public static DateTime ToDateTime(this DateOnly ConvertDate)
         {
             var NewDateTime = new DateTime(ConvertDate.Year, ConvertDate.Month, ConvertDate.Day);
             return NewDateTime;
         }
-        public static DateOnly ToDateOnly(DateTime ConvertDateTime)
+        public static DateOnly ToDateOnly(this DateTime ConvertDateTime)
         {
             var NewDateOnly = DateOnly.FromDateTime(ConvertDateTime);
             return NewDateOnly;
